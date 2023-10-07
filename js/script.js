9b4c1f906478a8bf0edff74864f7e71fb63ed409
Khushi110803 <122653550+Khushi110803@users.noreply.github.com>
2023-10-07 01:39:36 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
+// Define a function named 'changeColor' that will be called when the input elements change
 function changeColor()
 {
+     // Get the current value of the red, green, and blue range input elements
     let red = document.getElementById("Rrange").value;
     let green = document.getElementById("Grange").value;
     let blue = document.getElementById("Brange").value;
-    let color = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    let color = 'rgb(' + red + ',' + green + ',' + blue + ')';    // Create a string representing the RGB color
+
+    // Change the background color of the body to the newly computed color
     document.body.style.backgroundColor = color;
+
+    // Display the color value as text in an element with the id 'output'
     document.getElementById('output').innerHTML = ' : ' + color; 
 }
 
+// Add an event listener to the RGB range input element to call 'changeColor' when it changes
 document.getElementById('Rrange').addEventListener('input', changeColor);
 document.getElementById('Grange').addEventListener('input', changeColor);
 document.getElementById('Brange').addEventListener('input', changeColor);
 
+// Call 'changeColor' initially to set the background color and display the initial color value
 changeColor();
